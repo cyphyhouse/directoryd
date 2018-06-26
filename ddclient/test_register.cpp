70ccdcc95cdf98e2cc6c56a943cc4b5cd3739e3b
yholivas <yholivas@gmail.com>
2018-06-26 15:08:41 -0500

    testing acknowledgement messaging

--- ddclient/test_register.cpp
+++ ddclient/test_register.cpp
@@ -1,14 +1,35 @@
 #include <iostream>
+#include <sstream>
 #include <string>
+#include <zmq.hpp>
 #include "register.hpp"
 
 int main()
 {
-    for (int i = 0; i < 15; ++i)
-    {
+    zmq::context_t context (1);
+    zmq::socket_t subscriber (context, ZMQ_SUB);
+    subscriber.connect("tcp://localhost:5556");
+
+    const char * filter = "stop ";
+    subscriber.setsockopt(ZMQ_SUBSCRIBE, filter, strlen(filter));
+
+    int flag = 1;
+
+    do {
+        zmq::message_t update;
+        std::string signal;
+
         DDClient::register_service("quadcopter1", 1234, {{"type", "quadcopter"}});
+
+        subscriber.recv(&update);
+
+        std::istringstream iss(static_cast<char *>(update.data()));
+        iss >> signal;
+
+        flag = signal.compare("stop 1");
+
         sleep(1);
-    }
+    } while (flag != 0);
     return 0;
 }
 
