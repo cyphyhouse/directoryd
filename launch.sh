217df73e864ce3a85dca8dfc4a2c88aea1708067
ritwika314 <mim.ghosh@gmail.com>
2018-07-18 16:09:52 -0500

    update

--- launch.sh
+++ launch.sh
@@ -4,18 +4,12 @@ python botinfoupdate.py 1 192.168.1.19
 python testmainupdate.py 1 192.168.1.19
 cp TestMain.java ../src/main/java/testmain/
 cp BotInfoSelector.java ../src/main/java/testmain/
-mvn compile
-mvn clean install
-git add -A
-git commit -m "update"
-git push
+echo "192.168.1.19"
+./simpleclient 192.168.1.19echo "done"
 #add lines for functions for and waiting for messagespython botinfoupdate.py 2 192.168.1.18
 python testmainupdate.py 2 192.168.1.18
 cp TestMain.java ../src/main/java/testmain/
 cp BotInfoSelector.java ../src/main/java/testmain/
-mvn compile
-mvn clean install
-git add -A
-git commit -m "update"
-git push
+echo "192.168.1.18"
+./simpleclient 192.168.1.18echo "done"
 #add lines for functions for and waiting for messages
